4e3542fb86a03ef0a266f23fe025f6aec55501b8
Violet <furrychemistry@gmail.com>
2022-01-08 12:02:50 -0600

    Now uses HashSet

--- TS404/EntityCore/Entity.ComponentCollection.cs
+++ TS404/EntityCore/Entity.ComponentCollection.cs
@@ -51,13 +51,12 @@ public partial class Entity : IEntity, IEquatable<Entity>
 		}
 
 		private readonly Entity m_Entity;
-		private List<Component?> m_List = new();
-		private int m_Version;
+		private readonly HashSet<Component> m_Collection = new(5); // Use a prime number for capacity (see .net code).
 
 		/// <summary>
 		///		Number of <see cref="Component"/> contained.
 		/// </summary>
-		public int Count => m_List.Count;
+		public int Count => m_Collection.Count;
 
 		bool ICollection<Component>.IsReadOnly => false;
 
@@ -80,7 +79,7 @@ public partial class Entity : IEntity, IEquatable<Entity>
 		{
 			if (TTypeInfo<T>.CanBeComponent)
 			{
-				foreach (var component in m_List)
+				foreach (var component in m_Collection)
 				{
 					if (component is T cast)
 					{
@@ -102,17 +101,34 @@ public partial class Entity : IEntity, IEquatable<Entity>
 		///		New component of type <typeparamref name="T"/> could not be added.
 		/// </exception>
 		public T GetOrCreate<T>() where T : Component, new()
+			=> TryGetOrCreate<T>(out var result) ? result : throw new Exception("Could not add new component.");
+
+		/// <summary>
+		///		Returns <see langword="true"/> if a component matching <typeparamref name="T"/> was found or created,
+		///		otherwise returns <see langword="false"/>.
+		/// </summary>
+		public bool TryGetOrCreate<T>([NotNullWhen(true)] out T? result) where T : Component, new()
 		{
-			foreach (T? cast in m_List) { if (cast is not null) return cast; }
-			var component = new T();
-			return Add(component) ? component : throw new Exception($"New component of type {typeof(T).FullName} could not be added.");
+			foreach(T component in this)
+			{
+				if (component is not null)
+				{
+					result = component;
+					return true;
+				}
+			}
+
+			if (Add(result = new())) return true;
+
+			result = null;
+			return false;
 		}
 
 		/// <summary>
 		///		Returns <see langword="true"/> if <paramref name="component"/> is contained, otherwise
 		///		returns <see langword="false"/>.
 		/// </summary>
-		public bool Contains([NotNullWhen(true)] Component? component) => component is not null && component.Entity == m_Entity;
+		public bool Contains([NotNullWhen(true)] Component? component) => component is not null && m_Collection.Contains(component);
 
 		bool ICollection<Component>.Contains(Component component) => Contains(component);
 
@@ -125,19 +141,12 @@ public partial class Entity : IEntity, IEquatable<Entity>
 				return false;
 
 			bool removed = false;
-			lock (m_List)
+			lock (m_Collection)
 			{
 				if (component.Entity == m_Entity && (force || m_Entity.ComponentRemoveValidation(component)))
 				{
-					int index = m_List.IndexOf(component);
-					int lastIndex = Count - 1;
-
-					if (index != lastIndex) m_List[index] = m_List[lastIndex];
-					m_List.RemoveAt(lastIndex);
-					unchecked { m_Version++; }
-
+					m_Collection.Remove(component);
 					((IComponent)component).Entity = null;
-
 					removed = true;
 				}
 			}
@@ -167,15 +176,12 @@ public partial class Entity : IEntity, IEquatable<Entity>
 				return false;
 
 			bool added = false;
-			lock (m_List)
+			lock (m_Collection)
 			{
 				if (component.Entity is null && m_Entity.ComponentAddValidation(component))
 				{
-					m_List.Add(component);
-					unchecked { m_Version++; }
-
+					m_Collection.Add(component);
 					((IComponent)component).Entity = m_Entity;
-
 					added = true;
 				}
 			}
@@ -188,101 +194,47 @@ public partial class Entity : IEntity, IEquatable<Entity>
 		void ICollection<Component>.Add(Component component) { if (!Add(component)) throw new Exception("Component not added."); }
 
 		/// <summary>
-		///		Removed all contained <see cref="Component"/>.
+		///		Removes all contained <see cref="Component"/> except those that fail
+		///		<see cref="Entity.ComponentRemoveValidation(Component)"/>.
 		/// </summary>
 		public void Clear()
 		{
-			List<Component?> removed;
+			HashSet<Component> remove;
 
-			lock (this) // Entity.ComponentRemoveValidation might throw...
+			lock (m_Collection) // Entity.ComponentRemoveValidation might throw...
 			{
-				removed = m_List;
-				m_List = new();
-				for (int i = 0; i < removed.Count; i++)
+				remove = new(Count);
+				foreach (var component in m_Collection)
 				{
-					if (!m_Entity.ComponentRemoveValidation(removed[i]!))
+					if (!m_Entity.ComponentRemoveValidation(component))
 					{
-						m_List.Add(removed[i]);
-						removed[i] = null;
+						((IComponent)component).Entity = null;
+						remove.Add(component);
 					}
 				}
+				m_Collection.ExceptWith(remove);
 			}
 
-			foreach (var component in removed)
-			{
-				if (component is null) continue;
-				if (component.Entity == m_Entity) ((IComponent)component).Entity = null;
-				m_Entity.OnComponentRemoved(component);
-			}
+			foreach (var component in remove) m_Entity.OnComponentRemoved(component);
 		}
-
-		/// <summary>
-		///		Creates a new array of all contained <see cref="Component"/>.
-		/// </summary>
-		public Component?[] ToArray() => m_List.ToArray();
 
 		public void CopyTo(Component?[] array, int arrayIndex = 0)
 		{
 			const string ERR_NotEnoughSpace = "Array cannot hold all items.";
 			if (arrayIndex + Count >= array.Length) throw new ArgumentException(ERR_NotEnoughSpace);
 
-			Monitor.Enter(m_List);
-			if (arrayIndex + Count >= array.Length)
-			{
-				Monitor.Exit(m_List);
-				throw new ArgumentException(ERR_NotEnoughSpace);
-			}
-			m_List.CopyTo(array, arrayIndex);
-			Monitor.Exit(m_List);
+			Monitor.Enter(m_Collection);
+			if (arrayIndex + Count >= array.Length) throw new ArgumentException(ERR_NotEnoughSpace);
+#nullable disable
+			m_Collection.CopyTo(array, arrayIndex);
+#nullable restore
+			Monitor.Exit(m_Collection);
 		}
 
-		public IEnumerator<Component> GetEnumerator() { foreach (var component in m_List) yield return component!; }
+		public HashSet<Component>.Enumerator GetEnumerator() => m_Collection.GetEnumerator();
+
+		IEnumerator<Component> IEnumerable<Component>.GetEnumerator() => GetEnumerator();
 
 		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
-
-		public struct Enumerator : IEnumerator<Component>
-		{
-			private const string ERR_BadVersion = "Collection has changed.";
-			private readonly ComponentCollection m_Collection;
-			private readonly int m_Version;
-			private int m_Index = 0;
-			private Component? m_Current = null;
-
-			public Component Current => m_Current ?? throw new InvalidOperationException();
-			object IEnumerator.Current => Current;
-
-			internal Enumerator(ComponentCollection collection)
-			{
-				m_Collection = collection;
-				m_Version = collection.m_Version;
-			}
-
-			void IDisposable.Dispose() { }
-
-			public void Reset()
-			{
-				if (m_Version != m_Collection.m_Version) throw new InvalidOperationException(ERR_BadVersion);
-				m_Index = 0;
-				m_Current = null;
-			}
-
-			public bool MoveNext()
-			{
-				if (m_Version != m_Collection.m_Version)
-				{
-					throw new InvalidOperationException(ERR_BadVersion);
-				}
-				else if (m_Index < m_Collection.Count)
-				{
-					m_Current = m_Collection.m_List[m_Index++];
-					return true;
-				}
-				else
-				{
-					m_Current = null;
-					return false;
-				}
-			}
-		}
 	}
 }
